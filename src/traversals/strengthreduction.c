4ff1ba27f78662ebee7ad448f0341168e4413508
CollinHvA <collin.poetoehena@hva.nl>
2023-02-10 14:45:41 +0100

    Added first traversal (without implementation)

---
+++ src/traversals/strengthreduction.c
@@ -0,0 +1,76 @@
+/**
+ * @file
+ *
+ * Traversal: StrengthReduction
+ * UID      : SR
+ *
+ *
+ */
+
+#include "ccn/ccn.h"
+#include "ccngen/ast.h"
+
+/**
+ * @fn SRstmts
+ */
+node_st *SRstmts(node_st *node)
+{
+    return node;
+}
+
+/**
+ * @fn SRassign
+ */
+node_st *SRassign(node_st *node)
+{
+    return node;
+}
+
+/**
+ * @fn SRbinop
+ */
+node_st *SRbinop(node_st *node)
+{
+    return node;
+}
+
+/**
+ * @fn SRvarlet
+ */
+node_st *SRvarlet(node_st *node)
+{
+    return node;
+}
+
+/**
+ * @fn SRvar
+ */
+node_st *SRvar(node_st *node)
+{
+    return node;
+}
+
+/**
+ * @fn SRnum
+ */
+node_st *SRnum(node_st *node)
+{
+    return node;
+}
+
+/**
+ * @fn SRfloat
+ */
+node_st *SRfloat(node_st *node)
+{
+    return node;
+}
+
+/**
+ * @fn SRbool
+ */
+node_st *SRbool(node_st *node)
+{
+    return node;
+}
+
