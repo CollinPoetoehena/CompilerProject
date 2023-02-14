bebd914e295287f7fca97e9ab30c9950fe3434a0
CollinHvA <collin.poetoehena@hva.nl>
2023-02-14 08:48:45 +0100

    Some changes to 1.5

--- src/traversals/countoperators.c
+++ src/traversals/countoperators.c
@@ -18,6 +18,29 @@ void COfini() { return; }
  */
 node_st *CObinop(node_st *node)
 {
+    int addCount = 0;
+    int subCount = 0;
+    int mulCount = 0;
+    int divCount = 0;
+    int modCount = 0;
+
+    //TODO: how can I store this in the module node?
+    if (BINOP_TYPE(node) == BO_add) {
+        addCount++;
+    }
+    if (BINOP_TYPE(node) == BO_sub) {
+        subCount++;
+    }
+    if (BINOP_TYPE(node) == BO_mul) {
+        mulCount++;
+    }
+    if (BINOP_TYPE(node) == BO_div) {
+        divCount++;
+    }
+    if (BINOP_TYPE(node) == BO_mod) {
+        modCount++;
+    }
+
     return node;
 }
 
