b7630d23a56abc00b947dd9993191cbd2091c0c7
CollinHvA <collin.poetoehena@hva.nl>
2023-02-14 18:30:59 +0100

    Finished 1.4

--- src/traversals/strengthreduction.c
+++ src/traversals/strengthreduction.c
@@ -18,29 +18,27 @@ node_st *SRbinop(node_st *node)
     node_st *new = NULL;
 
     if (BINOP_TYPE(node) == BO_mul) {
-        if ((NODE_TYPE(BINOP_LEFT(node)) == NT_NUM || NODE_TYPE(BINOP_RIGHT(node)) == NT_NUM) &&
-            (NODE_TYPE(BINOP_LEFT(node)) == NT_VAR || NODE_TYPE(BINOP_RIGHT(node)) == NT_VAR)) {
-            // Both Binop children need to be a num value
-            node_st *leftNum = BINOP_LEFT(node);
-            node_st *rightNum = BINOP_RIGHT(node);
+        //CCNCopy 
+        node_st *left = CCNcopy(BINOP_LEFT(node));
+        node_st *leftTwo = CCNcopy(BINOP_LEFT(node));
+        node_st *right = CCNcopy(BINOP_RIGHT(node));
+        node_st *rightTwo = CCNcopy(BINOP_RIGHT(node));
 
-            if (NODE_TYPE(BINOP_LEFT(node)) == NT_VAR && NODE_TYPE(BINOP_RIGHT(node)) == NT_NUM) {
-
-            }
-
-            if (NUM_VAL(leftNum) == 2) {
-                new = ASTbinop(rightNum, rightNum, BO_add);
-            } else if (NUM_VAL(rightNum) == 2) {
-                new = ASTbinop(leftNum, leftNum, BO_add);
-            } else if (NUM_VAL(leftNum) == 3) {
-                new = ASTbinop(ASTbinop(leftNum, leftNum, BO_add), leftNum, BO_add);
-            } else if (NUM_VAL(leftNum) == 3) {
-                // TODO: Change k*3 to k+k+k add new Binop because 
-                // Expr (child type of Binop) can also be of type Binop
-                // 3 * k -> k+k+k
-                new = ASTbinop(ASTbinop(rightNum, rightNum, BO_add), rightNum, BO_add);
-            }
-        }
+        if (NODE_TYPE(left) == NT_VAR && 
+            (NODE_TYPE(right) == NT_NUM && NUM_VAL(right) == 2)) {
+                // Use one deep copy and one reference because otherwise
+                // you have children who point to the same node_st
+                new = ASTbinop(left, BINOP_LEFT(node), BO_add);
+        } else if (NODE_TYPE(right) == NT_VAR && 
+            (NODE_TYPE(left) == NT_NUM && NUM_VAL(left) == 2)) {
+                new = ASTbinop(right, BINOP_RIGHT(node), BO_add);
+        } else if (NODE_TYPE(left) == NT_VAR && 
+            (NODE_TYPE(right) == NT_NUM && NUM_VAL(right) == 3)) {
+                new = ASTbinop(ASTbinop(left, leftTwo, BO_add), BINOP_LEFT(node), BO_add);
+        } else if (NODE_TYPE(right) == NT_VAR && 
+            (NODE_TYPE(left) == NT_NUM && NUM_VAL(left) == 3)) {
+                new = ASTbinop(ASTbinop(right, rightTwo, BO_add), BINOP_RIGHT(node), BO_add);
+        } 
     }
 
     // return new Binop
