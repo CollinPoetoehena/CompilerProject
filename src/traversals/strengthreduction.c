b3851255be744135a003f05552c1043c5fc528d6
CollinHvA <collin.poetoehena@hva.nl>
2023-02-15 08:59:16 +0100

    Fixed issue with 1.4

--- src/traversals/strengthreduction.c
+++ src/traversals/strengthreduction.c
@@ -24,31 +24,29 @@ node_st *SRbinop(node_st *node)
         node_st *right = CCNcopy(BINOP_RIGHT(node));
         node_st *rightTwo = CCNcopy(BINOP_RIGHT(node));
 
-        // TODO: error:
-        // it gets to the if statements, however, it probably does not create the ASTbinop correct
         if (NODE_TYPE(left) == NT_VAR && 
             (NODE_TYPE(right) == NT_NUM && NUM_VAL(right) == 2)) {
                 // Use one deep copy and one reference because otherwise
                 // you have children who point to the same node_st
-                printf("Hoi\n");
                 new = ASTbinop(left, BINOP_LEFT(node), BO_add);
-                printf("%s", BINOP_LEFT(new));
-        } 
-        else if (NODE_TYPE(right) == NT_VAR && 
+        } else if (NODE_TYPE(right) == NT_VAR && 
             (NODE_TYPE(left) == NT_NUM && NUM_VAL(left) == 2)) {
-                printf("Hoi 2\n");
                 new = ASTbinop(right, BINOP_RIGHT(node), BO_add);
         } else if (NODE_TYPE(left) == NT_VAR && 
             (NODE_TYPE(right) == NT_NUM && NUM_VAL(right) == 3)) {
-                printf("Hoi 3\n");
                 new = ASTbinop(ASTbinop(left, leftTwo, BO_add), BINOP_LEFT(node), BO_add);
         } else if (NODE_TYPE(right) == NT_VAR && 
             (NODE_TYPE(left) == NT_NUM && NUM_VAL(left) == 3)) {
-                printf("Hoi 4\n");
                 new = ASTbinop(ASTbinop(right, rightTwo, BO_add), BINOP_RIGHT(node), BO_add);
         } 
     }
 
-    // return new Binop
-    return new;
+    if (new != NULL) {
+        // Return new Binop if changes were made
+        return new;
+    }
+
+    // Return node if no changes were made
+    return node;
+    
 }
