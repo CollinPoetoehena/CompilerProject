655042f32876547c1aaa7019c2e9260c302a88d5
CollinHvA <collin.poetoehena@hva.nl>
2023-02-15 17:18:33 +0100

    Finished assignment 1.6

--- src/traversals/countidentifiers.c
+++ src/traversals/countidentifiers.c
@@ -36,11 +36,13 @@ node_st *CIvar(node_st *node)
     struct data_ci *data = DATA_CI_GET();
 
     // Get the value from the identifier from the hash table
-    void *value = HTlookup(data->id_table, VAR_NAME(node));
+    int *value = (int *) HTlookup(data->id_table, VAR_NAME(node));
 
     if (value == NULL) {
         // Cast to void * because the parameter is of type void *
-        HTinsert(data->id_table, VAR_NAME(node), (void *) 1); // correct way
+        int *newValue = MEMmalloc(sizeof(int));
+        *newValue = 1;
+        HTinsert(data->id_table, VAR_NAME(node), (void *) newValue); // correct way
 
         // Save the variable in global memory (TODO: change without global variable, not permitted)
         // struct identifierCounter *id = MEMmalloc(sizeof(struct identifierCounter));
@@ -49,9 +51,11 @@ node_st *CIvar(node_st *node)
         // id->counter = 1;
     } else {
         // Remove the HT entry and re-add it
-        HTremove(data->id_table, VAR_NAME(node));
+        // HTremove(data->id_table, VAR_NAME(node));
 
-        HTinsert(data->id_table, VAR_NAME(node), (void *) ((int *)value)+1);
+        // dereference value and increment it by 1
+        *value = *value + 1;
+        // HTinsert(data->id_table, VAR_NAME(node), (void *) (*value)+1);
 
         // cast void * back to actual type
         // struct identifierCounter *id = (struct identifierCounter *) value;
@@ -60,8 +64,6 @@ node_st *CIvar(node_st *node)
         // Add the hash table to the travdata
         // data->id_table = hash_table;
     }
-
-        MODULE_ID_TABLE(node) = data->id_table;
 
     return node;
 }
@@ -75,6 +77,34 @@ node_st *CIvarlet(node_st *node)
     struct data_ci *data = DATA_CI_GET();
 
     // TODO: update the hash table
+
+    return node;
+}
+
+void * printElement(void * key, void * item) {
+    // Dereference int with * operator, char * == string
+    printf("%s %d\n", (char *) key, *(int *) item);
+}
+
+/**
+ * @fn CIvarlet
+ */
+node_st *CImodule(node_st *node)
+{
+    TRAVchildren(node);
+
+    // Get travdata from CI traversal
+    struct data_ci *data = DATA_CI_GET();
+
+    HTmapWithKey(data->id_table, printElement);
+
+    // htable_ptr hashTable = data->id_table;
+    // struct htable_entry entry = hashTable->entries;
+    // while (entry) {
+    //   printf("%s %d", (char *) entry->key, (int) entry->value);
+    //   // Create a new entry, at the end the entry will be NULL
+    //   entry = entry->next;
+    // }
 
     return node;
 }
