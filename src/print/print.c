36acf9bd66f9ff8252bd50378d449bb0a51925dc
CollinHvA <collin.poetoehena@hva.nl>
2023-02-17 09:02:47 +0100

    Small changes and tested everything, all works perfect!

--- src/print/print.c
+++ src/print/print.c
@@ -18,11 +18,11 @@
  */
 node_st *PRTmodule(node_st *node)
 {
-    printf("Add: %d\n", MODULE_ADD(node));
-    printf("Sub: %d\n", MODULE_SUB(node));
-    printf("Mul: %d\n", MODULE_MUL(node));
-    printf("Div: %d\n", MODULE_DIV(node));
-    printf("Mod: %d\n", MODULE_MOD(node));
+    printf("Add count: %d\n", MODULE_ADD(node));
+    printf("Sub count: %d\n", MODULE_SUB(node));
+    printf("Mul count: %d\n", MODULE_MUL(node));
+    printf("Div count: %d\n", MODULE_DIV(node));
+    printf("Mod count: %d\n", MODULE_MOD(node));
 
     TRAVstatements(node);
     return node;
