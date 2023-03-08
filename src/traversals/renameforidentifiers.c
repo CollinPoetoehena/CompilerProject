e24210e7e49581f8f19886cd489d9132a6b9325d
CollinHvA <collin.poetoehena@hva.nl>
2023-03-08 16:55:57 +0100

    Start on converting for loop ids

--- src/traversals/renameforidentifiers.c
+++ src/traversals/renameforidentifiers.c
@@ -9,10 +9,16 @@
 
 #include "ccn/ccn.h"
 #include "ccngen/ast.h"
+// Palm library for easy working with strings
+#include "palm/str.h"
 #include  <string.h>
 
 // Global counter for renaming the identifiers
 int counter = 1;
+// Global currentRenamedId is used to rename the occurrences of the old for loop id
+char *currentRenamedId = NULL;
+// Global currentForNode is used to save the new for loop node in and 
+node_st *currentForNode = NULL;
 
 /**
  * @fn RFIfor
@@ -26,13 +32,30 @@ node_st *RFIfor(node_st *node)
     // can be done in a separate traversal
     // dan simpel gewoon hier uitvoern met Ste
 
-    // Update the counter
-    char *previousId = FOR_VAR(node);
-    FOR_VAR(node) = MEMmalloc(strlen(previousId) + sizeof(int) + 2); // allocate memory for a string of up to 99 characters
-    // Initialize with empty string to avoid weird memory address value being used at the start
-    sprintf(FOR_VAR(node), "%s%d_", previousId, counter);
+    // Save the old for loop id
+    currentRenamedId = FOR_VAR(node);
 
-    printf("for loop identifier: %s\n", FOR_VAR(node));
+    // Rename the identifier of the For node (do not create a new node, this is much easier!)
+    // FOR_VAR(node) + counter + "_"
+
+    // Use for loop and concat an '_' for count times
+    for( int i = 0; i < counter; i++ ){
+        FOR_VAR(node) = STRcat(FOR_VAR(node), "_");
+    }
+ 
+    // // plak integers
+    // char *previousId = FOR_VAR(node);
+    // FOR_VAR(node) = MEMmalloc(strlen(previousId) + sizeof(int) + 2); // allocate memory for a string of up to 99 characters
+    // // Initialize with empty string to avoid weird memory address value being used at the start
+    // sprintf(FOR_VAR(node), "%s%d_", previousId, counter);
+
+    // printf("for loop identifier: %s\n", FOR_VAR(node));
+
+    // Save the For node in the global helper variable
+    currentForNode = node;
+
+    // Update the counter
+    counter++;
 
     return node;
 }
@@ -42,6 +65,15 @@ node_st *RFIfor(node_st *node)
  */
 node_st *RFIvar(node_st *node)
 {
+    // TODO: also rename the occurrence of the for identifier
+    // Test if it is not renaming other occurrences of i
+    if (currentRenamedId != NULL && currentForNode != NULL) {
+        if (strcmp(FOR_VAR(currentForNode), VAR_NAME(node)) == 0) {
+            // Create a copy of the id to avoid pointing to the same id of the For node
+            char *newVarName = STRcpy(FOR_VAR(currentForNode));
+            VAR_NAME(node) = newVarName;
+        }
+    }
 
     return node;
 }
@@ -51,6 +83,15 @@ node_st *RFIvar(node_st *node)
  */
 node_st *RFIvarlet(node_st *node)
 {
+    // TODO: also rename the occurrence of the for identifier
+    // TODO: check if it does not rename other identifiers not from the for loop
+    if (currentRenamedId != NULL && currentForNode != NULL) {
+        if (strcmp(FOR_VAR(currentForNode), VARLET_NAME(node)) == 0) {
+            // Create a copy of the id to avoid pointing to the same id of the For node
+            char *newVarLetName = STRcpy(FOR_VAR(currentForNode));
+            VARLET_NAME(node) = newVarLetName;
+        }
+    }
 
     return node;
 }
