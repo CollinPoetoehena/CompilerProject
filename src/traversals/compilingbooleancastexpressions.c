2f6ba58302a3db19054f070d458093c0c89dbf2e
CollinHvA <collin.poetoehena@hva.nl>
2023-03-11 14:19:05 +0100

    Created CBCE traversal and added test file

---
+++ src/traversals/compilingbooleancastexpressions.c
@@ -0,0 +1,20 @@
+/**
+ * @file
+ *
+ * Traversal: CompilingBooleanCastExpressions
+ * UID      : CBCE
+ *
+ *
+ */
+
+#include "ccn/ccn.h"
+#include "ccngen/ast.h"
+
+/**
+ * @fn CBCEcast
+ */
+node_st *CBCEcast(node_st *node)
+{
+    return node;
+}
+
