74938189d798435ce0af18c180af26c8b24cfdf2
CollinHvA <collin.poetoehena@hva.nl>
2023-03-11 10:45:05 +0100

    Converting VarDecls into regular assignments finished, now do GlobDefs

--- src/traversals/regularassignments.c
+++ src/traversals/regularassignments.c
@@ -42,6 +42,7 @@ node_st *currentGlobdefStmts = NULL;
 // node_st * global variables are used for building and updating the new nodes
 node_st *firstVardeclStmts = NULL;
 node_st *currentVardeclStmts = NULL;
+bool firstStmtsNode = true;
 
 // vardecls in een lijst
 // __init maak je aan met ASTfundef, alle initializaties, worden stmts, alleen voor globdef hoeft dit
@@ -59,6 +60,8 @@ void updateGlobDefStmts(node_st *newStmts) {
             STMTS_NEXT(currentGlobdefStmts) = newStmts;
             currentGlobdefStmts = newStmts;
         } else {
+            // // First create a new Stmts node
+            // node_st *newStmtsNode = ASTstmts(newStmts, NULL);
             // Save it as the first Stmts
             firstGlobdefStmts = newStmts;
             currentGlobdefStmts = newStmts;
@@ -92,8 +95,16 @@ void updateVarDeclStmts(node_st *newStmts) {
  */
 node_st *RAprogram(node_st *node)
 {
-    // TODO: maybe start with the other part, the vardecls, then focus on the __init function or leave it until class!
+    // First traverse the GlobDefs to convert the initializations
     TRAVdecls(node);
+
+    // Update the Stmts of the funbody
+    if (firstGlobdefStmts != NULL && currentGlobdefStmts != NULL) {
+        // First update the last Decls node of the GlobDef initializations with the current Decls
+        DECLS_NEXT(currentGlobdefStmts) = PROGRAM_DECLS(node);
+        // Then update the programs's decls with the new first Decls (which also contains the old Decls chain)
+        PROGRAM_DECLS(node) = firstGlobdefStmts;
+    }
 
     return node;
 }
@@ -155,6 +166,17 @@ node_st *RAfunbody(node_st *node)
     // First traverse the VarDecls to convert the initializations
     TRAVdecls(node);
 
+    // Set first stmts node to true because you are entering a new set of Stmts in a new funbody
+    // firstStmtsNode = true;
+
+    // Update the Stmts of the funbody
+    if (firstVardeclStmts != NULL && currentVardeclStmts != NULL) {
+        // First update the last Stmts node of the VarDecls initializations with the current stmts
+        STMTS_NEXT(currentVardeclStmts) = FUNBODY_STMTS(node);
+        // Then update the funbody's stmts with the new first Stmts (which also contains the old stmts chain)
+        FUNBODY_STMTS(node) = firstVardeclStmts;
+    }
+
     // Then traverse the Stmts and prepend the new Stmts nodes collected from the VarDecls
     TRAVstmts(node);
 
@@ -171,22 +193,24 @@ node_st *RAfunbody(node_st *node)
 node_st *RAvardecl(node_st *node)
 {
     // If the vardecl has an initialization, convert it
-    if (VARDECL_INIT(node) != NULL) {
-        // First convert it to a Varlet node and an Assign node
-        //TODO: what to do with the Link attribute, probably this traversal should be before the CA right?!
+    if (VARDECL_INIT(node) != NULL) {        
         // Create copies to avoid pointing to the same reference twice
         char *copiedVarDeclName = STRcpy(VARDECL_NAME(node));
-        node_st *newVarletNode = ASTvarlet(copiedVarDeclName);
-        node_st *copiedVarDeclExpr = CCNcopy(VARDECL_INIT(node));
 
-        node_st *newAssignNode = ASTassign(newVarletNode, copiedVarDeclExpr);
+        // Create new VarLet, Assign and Stmts node, use the current INIT Expr node
+        // TODO: is this correct, no copies required or????
+        // TODO: what to do with the Link attribute, probably this traversal should be before the CA right?!
+        node_st *newStmtsNodeVarDecl = ASTstmts(ASTassign(ASTvarlet(copiedVarDeclName), VARDECL_INIT(node)), NULL);
 
-        // Add the new AssignNode to the global Stmts helper variable
-        updateVarDeclStmts(newAssignNode);
+        // Add the new AssignNode to the global Stmts helper variables
+        updateVarDeclStmts(newStmtsNodeVarDecl);
 
         // Then update this VarDecl node by setting the initialization to NULL
-       VARDECL_INIT(node) = NULL;
+        VARDECL_INIT(node) = NULL;
     }
+
+    // Go to the next VarDecls to traverse that ndoe
+    TRAVnext(node);
     
     return node;
 }
@@ -196,9 +220,9 @@ node_st *RAvardecl(node_st *node)
  */
 node_st *RAstmts(node_st *node)
 {
-    // Update statements by prepending new Stmts node to the first one occurring
+    // TODO: does this need to be here or is nothing done with the stmts??
+    TRAVstmt(node);
 
-    // Reset first Stmts boolean to only prepend to the first one
 
     return node;
 }
