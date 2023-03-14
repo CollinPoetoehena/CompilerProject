154300f9a9e596319380b1e078d15b760c5f407b
CollinHvA <collin.poetoehena@hva.nl>
2023-03-14 09:19:41 +0100

    Added TODO's and cleanup + finished milestone 9

--- src/traversals/compilingbooleancastexpressions.c
+++ src/traversals/compilingbooleancastexpressions.c
@@ -20,10 +20,12 @@ node_st *CBCEcast(node_st *node)
     // See message from Simon about milestone 10, the assignment is to implement a new Ternary Operator node in the AST!
     // Add this node to the nodeset operation
 
+    // First traverse the expression to also convert those Cast nodes if they are Casts
+
 
     // If the Cast type is bool than it needs to be converted
     if (CAST_TYPE(node) == CT_bool) {
-
+        
     }
 
     // Or if the result features boolean operators it needs to be converted
--- src/traversals/compilingbooleandisjunctionandconjunction.c
+++ src/traversals/compilingbooleandisjunctionandconjunction.c
@@ -24,8 +24,16 @@ node_st *CBDCbinop(node_st *node)
     // Then, convert binop that is a logical operator to TernaryOp node when the children are traversed.
     // This is done after because otherwise a BinOp with a BinOp Expr would not be correctly converted to TernaryOp
     if (BINOP_OP(node) == BO_or) {        
-        // Use the type signature for the BinOp node obtained in TypeChecking
-        // pred == true ? then not evaluate then expr (true expr) : otherwise evaluate else expression to find true
+        /*
+        Use the type signature from the BinOp node obtained in TypeChecking
+
+        Format for new TernaryOp node for the BinOp ||:
+        predicate == true 
+        ? 
+        then not evaluate then expr because it already is true (== ASTbool with true value)
+        :
+        otherwise evaluate else expression to find if that returns true
+        */
         node_st *newConvertedNode = ASTternaryop(BINOP_LEFT(node), ASTbool(true), 
             BINOP_RIGHT(node), BINOP_OPERATOR_TYPE_SIGNATURE(node));
 
@@ -42,8 +50,16 @@ node_st *CBDCbinop(node_st *node)
         // Return the new TernaryOp node
         return newConvertedNode;
     } else if (BINOP_OP(node) == BO_and) {
-        // Use the type signature for the BinOp node obtained in TypeChecking
-        // pred == true ? then evaluate then expr to also find true : otherwise not evaluate else expression
+        /*
+        Use the type signature from the BinOp node obtained in TypeChecking
+
+        Format for new TernaryOp node for the BinOp &&:
+        predicate == true 
+        ? 
+        then evaluate then expression to find if that also returns true
+        :
+        otherwise not evaluate else expression because it is already false (== ASTbool with false value)
+        */  
         node_st *newConvertedNode = ASTternaryop(BINOP_LEFT(node), BINOP_RIGHT(node), 
             ASTbool(false), BINOP_OPERATOR_TYPE_SIGNATURE(node));
 
