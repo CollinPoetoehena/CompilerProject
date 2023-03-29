a6d02f3123fa14b234ad54e3c284e8da8acfd9c2
CollinHvA <collin.poetoehena@hva.nl>
2023-03-29 15:35:24 +0200

    Removed build-debug

--- build-debug/palm/include/palm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/poetoec/CompilerConstruction/CompilerProject/CompilerProject/coconut/palm/include/palm
-# Build directory: /home/poetoec/CompilerConstruction/CompilerProject/CompilerProject/build-debug/palm/include/palm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
